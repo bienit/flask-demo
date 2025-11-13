23d25d16c8bbfa56f63d349d5bc67a9ba22b0562
bienit <biendtvt@gmail.com>
2025-11-13 11:14:01 +0700

    update v2

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask App on Kubernetes with ArgoCD!"
+    return "Hello from Flask App on Kubernetes with ArgoCD! V2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
